3f6279dfe83971ccf748b5ed09837e0d8a476df9
ycp <1297815944@qq.com>
2021-11-21 16:11:22 +0800

    commit

--- src/main/java/com/ycp/git/Git.java
+++ src/main/java/com/ycp/git/Git.java
@@ -6,9 +6,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test");
-        System.out.println("git test");
-        System.out.println("git test");
-        System.out.println("git test");
+
 
     }
 }
